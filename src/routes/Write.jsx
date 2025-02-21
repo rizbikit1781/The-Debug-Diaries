eefafcec0678cec40707d76e8d8c5430f5600000
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2025-02-20 21:43:30 -0700

    Add Write

--- src/routes/Write.jsx
+++ src/routes/Write.jsx
@@ -1,5 +1,8 @@
 import { useUser } from "@clerk/clerk-react";
 import React from "react";
+import ReactQuill, { Quill } from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
+
 
 const Write = () => {
   const { isLoaded, isSignedIn } = useUser();
@@ -12,10 +15,10 @@ const Write = () => {
     return <div className="">Please login to write a content!</div>;
   }
   return (
-    <div className="">
-      <h1></h1>
-      <form action="">
-        <button>Add a cover image</button>
+    <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6">
+      <h1 className="text-xl font-light">Create a New Post</h1>
+      <form action="" className="flex flex-col gap-6 bg-red-100 flex-1 mb-6">
+        <button className="p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white">Add a cover image</button>
         <input type="text" placeholder="My Awesome Story" />
         <div className="">
           <label htmlFor="">Choose a category:</label>
@@ -28,6 +31,9 @@ const Write = () => {
             <option value="marketing">Marketing</option>
           </select>
         </div>
+        <textarea name="desc" placeholder="A Short Description" />
+        <ReactQuill theme="snow" />
+        <button>Send</button>
       </form>
     </div>
   );
