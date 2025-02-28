8e73d3328ab83c66ae71b0c182c79b577e0e96e2
Paradon Meeanan <112736862+rizbikit1781@users.noreply.github.com>
2025-02-27 17:36:10 -0700

    Added route

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import userRouter from "./routes/user.route.js"
+import postRouter from "./routes/post.route.js"
+import commentRouter from "./routes/commend.route.js"
 
 const app = express()
 
-app.get("/test", (req, res) => {
-    res.status(200).send("It works")
-})
+// app.get("/test", (req, res) => {
+//     res.status(200).send("It works")
+// })
 
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/commends", commentRouter);
 
 
 app.listen(3000, () => {
